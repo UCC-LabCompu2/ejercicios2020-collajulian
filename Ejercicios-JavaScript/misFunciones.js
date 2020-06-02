2131af7726caa6d46bd9436c5d1a29e0b8931774
collajulian <juliancolla16@gmail.com>
2020-06-02 15:48:37 -0300

    Ejercicio mini paint

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -145,3 +145,31 @@ ctx.fillStyle = "#840471";
 ctx.fill();
 
 }
+
+var bandera;
+
+function dibujar(event) {
+var canvas = document.getElementById("canvasAdibujar");
+var ctx = canvas.getContext("2d");
+
+var posX = event.clientX;
+var posY = event.clientY;
+console.log(posX,posY);
+
+canvas.onmousedown = function (){bandera=true};
+canvas.onmouseup = function (){bandera=false};
+
+if (bandera) {
+    ctx.fillRect(posX, posY, 5, 5);
+    ctx.fill;
+}
+
+}
+
+function LimpiarCanvas() {
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+
+}
